dc7da752744cc7782e3b7484e349d982e7d8c76f
aryanbagra <aryanbagra972@gmail.com>
2024-09-09 10:40:35 +0530

    Create fastclass.java

---
+++ fastclass.java
@@ -0,0 +1,16 @@
+public class fastclass {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("onilen gameing app<program all mobile phone logen");
+        System.out.println("ioss >\"indrode phone and laptop and (logen lincke ARYAN SHARMA@.COME)::\n" + //
+                        "");
+                        System.out.println("HELLO EVERONE may app donwede baten in lincke go and donwede ");
+                        System.out.println("(gameing crecket tanice batmantle casse tamplerun pubge mobile freefire<INDIA> CALL OFF DUTA.\n" + //
+                                                        "..ALL GAME PROVIDE IN APP NAME GREEN ONLINE GAMEING INDIA)barke gameing\n" + //
+                                                        "<app saystem> aper limeted brack \n" + //
+                                                        "<saystem >lemited theish app pro progrem all mobile upgred systm provide thish app for\n" + //
+                                                        "/////gameinge logine {<I'D@GMAIL.COM ARYANSHARMA >a.sbagra} \n" + //
+                                                        "}system brack :aneone");
+    }
+}
